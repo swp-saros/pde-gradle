5aff98a7d5b35060a442730237fc8cc37d3bb0a9
FKHals <5229803-FKHals@users.noreply.gitlab.com>
2021-01-11 23:51:41 +0100

    Redo workaround for build fail
    This removes the previous addition of org.eclipse.ui.workbench to the
    requirements in the manifest.mf and instead makes it a gradle dependency
    which makes it possible to define the version and in general more flexible.

--- ExampleFrontend/build.gradle.kts
+++ ExampleFrontend/build.gradle.kts
@@ -39,6 +39,9 @@ dependencies {
     implementation(project(":ExampleCore"))
     // This is a workaround for https://github.com/saros-project/saros/issues/1086
     implementation("org.eclipse.platform:org.eclipse.urischeme:1.1.0")
+    // This is a workaround for https://github.com/saros-project/saros/issues/1114
+    implementation("org.eclipse.platform:org.eclipse.ui.ide:3.17.200")
+    implementation("org.eclipse.platform:org.eclipse.ui.workbench:3.120.0")
 }
 
 tasks {
